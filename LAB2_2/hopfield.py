f64a26c181255f57526bfc25bedcf5cf5bc51557
Francesca Poli <francescapoli1998@gmail.com>
2024-04-18 09:47:54 +0200

    lab 2_2 improvements

--- LAB2_2/hopfield.py
+++ LAB2_2/hopfield.py
@@ -12,9 +12,9 @@ from numpy import einsum, fill_diagonal, random
 
 
 class Hopfield:
-    def __init__(self, input_data: np.array, patterns: np.array):
-        self.x = input_data #input_data (the original p0, p1, p2 then distorted into x0, x1, x2)
-        self.N =  self.x.shape[1] #number of neurons (1024)
+    def __init__(self, img: np.array, patterns: np.array):
+        self.x = img #input_data (the original p0, p1, p2 then distorted into x0, x1, x2)
+        self.N =  self.x.shape[0] #number of neurons (1024)
         self.p = patterns #set of patterns
         self.epochs = 100
         self.bias = None #TO BE CHANGED
@@ -34,47 +34,18 @@ class Hopfield:
         return w   
     
     
-    
-    def overlap_func(self, x):
-        """
-        The `overlap_func` function calculates the overlap of input `x` with patterns stored in
-        `self.p`.
-        
-        :param x: It looks like the `overlap_func` function takes two parameters: `self` and `x`. The
-        `x` parameter seems to be a value that is used in the function to calculate the overlap of
-        patterns stored in `self.p`. The function calculates the overlap of each pattern in `self.p
-        :return: The `overlap_func` function is returning a list `o` where each element is the dot
-        product of a pattern from `self.p` and the input `x`, divided by the total number of elements
-        `N` in the pattern.
-        """
-        o = [(pattern @ x / self.N) for pattern in self.p.T]
+    def overlap_func(self, x: np.array):
+        o = [(pattern @ x / self.N) for pattern in self.p]
         return o
     
-    def energy_func(self, x, w):
-        """
-        The function calculates the energy based on the input vector x and weight matrix w.
-        
-        :param x: The `x` parameter seems to represent a vector or matrix. The `@` operator is used for
-        matrix multiplication in this context
-        :param w: The parameter `w` in the `energy_func` function appears to be a weight matrix. It is
-        used in the calculation of the energy of the system based on the input `x`. The `@` operator is
-        likely performing matrix multiplication between `x` and `w` in this context
-        :return: The function `energy_func` returns the energy value calculated based on the input `x`
-        and weight `w`.
-        """
-        e = - x.T @ w @ x / 2 - sum(self.bias * x)
+    def energy_func(self, x: np.array, w: np.array):
+        e = - x.T @ w @ x / 2 #- sum(self.bias * x)
         return e
         
         
     def __call__(self):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
         w = self.storage()
-        x=self.x.T
-        print("Weight matrix: ", w.shape, "input matrix: ", x.shape)
+        x=self.x
         overlaps = []
         energies = []
         for epoch in range(self.epochs):
@@ -85,17 +56,10 @@ class Hopfield:
                 #get a random neuron and update its state through the update function
                 update = np.sign(w[ind] @ x) #ADD BIAS HERE
                 x[ind] = update
-                #overlap
-                self.overlap_func(update) 
-                #overlap = self.overlap_func(update)
-                #energy
-                energy = self.energy_func(update, w)
-                
-                overlaps.append(overlap)
-                energies.append(energy)
-        return overlaps, energies, x
-                
-                
-                
-                
-                
+            #overlap
+            overlap = self.overlap_func(x) 
+            #energy
+            energy = self.energy_func(x, w)
+        overlaps.append(overlap)
+        energies.append(energy)
+        print(overlaps, energies, x)

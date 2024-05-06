805108feb6bd9bfbe11498a9fee6cb9fb8f3811e
Francesca Poli <francescapoli1998@gmail.com>
2024-05-06 19:51:03 +0200

    intro to 3_1

--- LAB2_2/hopfield.py
+++ LAB2_2/hopfield.py
@@ -15,6 +15,18 @@ from numpy import einsum, fill_diagonal, random
 
 class Hopfield:
     def __init__(self, img: np.array, patterns: np.array):
+        """
+        This function initializes parameters for a neural network model with image data and patterns.
+        
+        :param img: The `img` parameter is expected to be a NumPy array representing the input data. In
+        this case, it seems to be the original data that has been distorted into different patterns
+        :type img: np.array
+        :param patterns: The `patterns` parameter is an array containing a set of patterns. In the
+        context of the code snippet you provided, it seems like these patterns are related to image data
+        processing or neural network training. The `patterns` array likely contains data that represents
+        specific features or characteristics that the neural network is trained
+        :type patterns: np.array
+        """
         self.x = img #input_data (the original p0, p1, p2 then distorted into x0, x1, x2)
         self.N =  self.x.shape[0] #number of neurons (1024)
         self.p = patterns #set of patterns
@@ -37,15 +49,43 @@ class Hopfield:
     
     
     def overlap_func(self, x: np.array):
+        """
+        The function `overlap_func` calculates the overlap of an input array with a set of patterns.
+        
+        :param x: The `x` parameter is expected to be a NumPy array that you will pass to the
+        `overlap_func` method. The method calculates the overlap of the input `x` with each pattern in
+        `self.p` and returns a list of overlap values
+        :type x: np.array
+        :return: The function `overlap_func` is returning a list `o` containing the dot product of each
+        pattern in `self.p` with the input array `x`, divided by the total number of elements `N` in
+        each pattern.
+        """
         o = [(pattern @ x / self.N) for pattern in self.p]
         return o
     
     def energy_func(self, x: np.array, w: np.array):
+        """
+        This function calculates the energy based on input arrays x and w using matrix multiplication.
+        
+        :param x: The parameter `x` is a numpy array representing the input data or features. It is
+        typically a vector or matrix containing the input values for a model
+        :type x: np.array
+        :param w: The parameter `w` in the `energy_func` function appears to be a numpy array. It is
+        used as a weight matrix in the calculation of energy. The function calculates the energy based
+        on the input `x` and the weight matrix `w`
+        :type w: np.array
+        :return: the energy value calculated based on the input arrays `x` and `w`.
+        """
         e = - x.T @ w @ x / 2 #- sum(self.bias * x)
         return e
         
         
     def __call__(self):
+        """
+        This function performs a stochastic update of neuron states over multiple epochs, calculating
+        overlap and energy functions at each step.
+        :return: three values: a list of overlaps, a list of energies, and the updated state vector x.
+        """
         w = self.storage()
         x=self.x
         overlaps = []
---
+++ LAB3_1/assignment 1/tdnn.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+''' 
+This is a PyTorch neural network model class that implements a simple TDNN architecture with a
+specified number of hidden layers and dimensions. 
+'''
+
+# class TDNN_model(nn.Module):
+#     def __init__():
+#       
+#     def forward():

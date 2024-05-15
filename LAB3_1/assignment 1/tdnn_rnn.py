ed4608c15ce16c0de17c805d241f741a25ac3613
Francesca Poli <francescapoli1998@gmail.com>
2024-05-15 17:52:46 +0200

    lab 3_1

---
+++ LAB3_1/assignment 1/tdnn_rnn.py
@@ -0,0 +1,28 @@
+''' 
+This is a PyTorch neural network model class that implements a simple RNN architecture with a
+specified number of hidden layers and dimensions. 
+'''
+# class RNN_model(nn.Module):
+#     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+#         super(RNN_model, self).__init__()
+        
+#         # Number of hidden dimensions
+#         self.hidden_dim = hidden_dim
+        
+#         # Number of hidden layers
+#         self.layer_dim = layer_dim
+        
+#         # RNN
+#         self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim, batch_first=True, nonlinearity='relu')
+        
+#         # Readout layer
+#         self.fc = nn.Linear(hidden_dim, output_dim)
+    
+#     # def forward(self, x):
+#     #     # Initialize hidden state with zeros
+#     #     h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
+            
+#     #     # One time step
+#     #     out, hn = self.rnn(x, h0)
+#     #     out = self.fc(out[:, -1, :]) 
+#     #     return out

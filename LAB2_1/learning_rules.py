94a022d82f723be56a2be73be83f4287cce01e18
Francesca Poli <francescapoli1998@gmail.com>
2024-07-14 12:24:08 +0200

    still fixing

--- LAB2_1/learning_rules.py
+++ LAB2_1/learning_rules.py
@@ -7,12 +7,14 @@ import matplotlib.pyplot as plt
 # The `HebbianLearning` class implements Hebbian learning rules such as Hebbian, Oja, and subtractive
 # normalization.
 class HebbianLearning:
-    def __init__(self, alpha=0, one=0, **kwargs):
-        self.w_init = np.random.uniform(low=-1, high=1, size=(2,))
+    def __init__(self, data_shape, alpha=0, one=0, theta=0.1, th_lr=0.01):
+        self.w_init = np.random.uniform(low=-1, high=1, size=data_shape) #np.random.uniform(data_shape, data_shape, size=(2,)) #np.random.uniform(low=-1, high=1, size=(2,))
         self.lr= 0.001
-        self.epochs=200
+        # self.epochs=200
         self.alpha = alpha #oja rule
         self.one = one #subtractive normalization
+        self.theta=theta #bcm
+        self.theta_lr=th_lr
 
     def hebbian_rule(self, u, v, w):
         w = w + (self.lr * u * v) 
@@ -26,5 +28,11 @@ class HebbianLearning:
         w = v * u - (v * (self.one.T @ u) * self.one) / 2
         return w
     
-    def 
+    def bcm(self, u, v, w):
+        v = w @ u
+        self.theta += self.theta_lr * (np.power(v, 2) - self.theta)
+        return v * u * (v - self.theta)
     
+    def covariance(self, u, v, w):
+        v = w @ u
+        return v * (u - self.theta)
